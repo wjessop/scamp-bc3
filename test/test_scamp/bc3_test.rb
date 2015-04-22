1966351e34758dadf2de796dc409ad2c5c1ef19d
Will Jessop <will@willj.net>
2015-04-22 11:13:20 +0100

    Lose the stub test

--- test/test_scamp/bc3_test.rb
+++ test/test_scamp/bc3_test.rb
@@ -1,11 +1,7 @@
 require 'minitest_helper'
 
-class TestScamp::Bc3 < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Scamp::Bc3::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
+class TestScampBc3 < Minitest::Test
+	def test_that_it_has_a_version_number
+		refute_nil ::Scamp::Bc3::VERSION
+	end
 end
